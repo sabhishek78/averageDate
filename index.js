43e63359489378a66451f6d8cae0e84c40492067
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2020-06-14 11:27:04 +0000

    code corrected

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ console.log(averageDate([
   new Date(Date.UTC(1970, 0, 1)),
   new Date(Date.UTC(1971, 0, 1))
 ]))
+// Create a function that given an array of dates returns the average date in string form.
+
+// Examples
+// averageDate([]) ➞ null
+
+// averageDate([
+//   new Date(Date.UTC(1970, 0, 1))
+// ]) ➞ "1970-01-01T00:00:00.000Z"
+
+// averageDate([
+//   new Date(Date.UTC(1970, 0, 1)),
+//   new Date(Date.UTC(1971, 0, 1))
+// ]) ➞ "1970-07-02T12:00:00.000Z"
+// Notes
+// For the return value you can use the .toISOString() method.
+// If dates is an empty array return null.
